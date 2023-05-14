fea0bc07a8c846ea98cd13afa792976f64e30bac
edytaskibinska <edyta.skibinska@gmail.com>
2023-05-14 18:58:43 +0200

    enhancing of about page (map on about.json data

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,18 +4,22 @@ import useFetch from "../../hooks/useFetchAsync";
 import mountain from "../../assets/images/backgroundMountains.png";
 
 function About() {
-  const currentData = useFetch("./data.json");
+  const currentData = useFetch("./about.json");
   const { data } = currentData;
-  const tempDesc = data[0]?.description;
 
   return (
     <article className="about">
       <Diaporama imgUrl={mountain} alt="mountain" />
-      <Block withPadding >
-        <Dropdown dropTitle="Fiabilité" dropContent={tempDesc} />
-        <Dropdown dropTitle="Respect" dropContent={tempDesc} />
-        <Dropdown dropTitle="Services" dropContent={tempDesc} />
-        <Dropdown dropTitle="Securité" dropContent={tempDesc} />
+      <Block withPadding>
+        {data.map((item, index) => {
+          return (
+            <Dropdown
+              key={index}
+              dropTitle={item.title}
+              dropContent={item.content}
+            />
+          );
+        })}
       </Block>
     </article>
   );
